814d251524fb3f2e19a3c1baf09bc4656c81ce6f
webhedo <khonjelwayo@gmail.com>
2021-04-16 12:15:10 +0200

    theme

--- assets/js/theme-change.js
+++ assets/js/theme-change.js
@@ -1,9 +1,9 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-const currentTheme = localStorage.getItem('theme');
+const currentTheme = 'dark';
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-  
+    
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
     }
